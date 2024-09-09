065e1658d74abe356d66f3ade7cd5009fee5ee3b
tohrxyz <tomas.hrib@proton.me>
2024-09-09 13:59:32 +0200

    prepare action and subject funcs

--- src/main.go
+++ src/main.go
@@ -25,6 +25,23 @@ func isWithinPossibilities(input string, possibilities []string) bool {
 	return isWithin
 }
 
+func create(subject string, name string) error {
+
+}
+func insert(subject string, name string) error {
+
+}
+func update(subject string, name string) error {
+
+}
+func delete(subject string, name string) error {
+
+}
+
+func table(subject string, name string, method string) error {
+
+}
+
 func main() {
 	args := os.Args[1:]
 	fmt.Println(args)
@@ -33,15 +50,38 @@ func main() {
 		panic("minimum num of args is 3 (action, subject, name)")
 	}
 
-	isValidAction := isWithinPossibilities(args[0], POSSIBLE_ACTIONS)
-	fmt.Printf("Is %s valid action: %t\n", args[0], isValidAction)
+	action := args[0]
+	isValidAction := isWithinPossibilities(action, POSSIBLE_ACTIONS)
+	fmt.Printf("Is %s valid action: %t\n", action, isValidAction)
 	if !isValidAction {
-		panic("%s is not a valid action")
+		panic(action + " " + "is not a valid action")
 	}
 
-	isValidSubject := isWithinPossibilities(args[1], POSSIBLE_SUBJECTS)
-	fmt.Printf("Is %s valid subject: %t\n", args[1], isValidSubject)
+	subject := args[1]
+	isValidSubject := isWithinPossibilities(subject, POSSIBLE_SUBJECTS)
+	fmt.Printf("Is %s valid subject: %t\n", subject, isValidSubject)
 	if !isValidAction {
-		panic("%s is not a valid subject")
+		panic(subject + " " + "is not a valid subject")
+	}
+
+	name := args[2]
+
+	switch action {
+	case POSSIBLE_ACTIONS[0]:
+		fmt.Println("creating...")
+		create(subject, name)
+		break
+	case POSSIBLE_ACTIONS[1]:
+		fmt.Println("updating...")
+		update(subject, name)
+		break
+	case POSSIBLE_SUBJECTS[2]:
+		fmt.Println("inserting...")
+		insert(subject, name)
+		break
+	case POSSIBLE_ACTIONS[3]:
+		fmt.Println("deleting...")
+		delete(subject, name)
+		break
 	}
 }
