f1cd1addaed62e15ded1895f2fa036f25e3309f9
tohrxyz <tomas.hrib@proton.me>
2024-09-10 18:36:59 +0200

    create table

--- src/lib/subjects.go
+++ src/lib/subjects.go
@@ -3,6 +3,33 @@ package lib
 import "fmt"
 
 func Table(subject string, name string, method string) error {
-	fmt.Println("table")
+	switch method {
+	case "CREATE":
+		err := DeclareTableConf(name)
+		if err != nil {
+			return err
+		}
+		break
+	}
+	return nil
+}
+
+func DeclareTableConf(tableName string) error {
+	filepathDir := FilepathFromTableName(tableName, true)
+	fmt.Printf("LOG: filepathDir: %v\n", filepathDir)
+	err := CreateDirIfNotExists(filepathDir, false)
+	if err != nil {
+		return err
+	}
+	fmt.Println("LOG: Directory for table " + tableName + " successfully created.")
+
+	filepath := FilepathFromTableName(tableName, false)
+	fmt.Printf("LOG: filepath for table file: %v\n", filepath)
+	err = CreateFileIfNotExists(filepath)
+	if err != nil {
+		return err
+	}
+	fmt.Println("LOG: File for table " + tableName + " successfully created.")
+
 	return nil
 }
